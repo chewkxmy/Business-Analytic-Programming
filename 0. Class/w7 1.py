5fc9e388d2a22e72dcfad8e03fb679753e1aa48a
Kai Xian <chewkxmy@gmail.com>
2024-05-09 09:22:11 +0800

    w7 Class

---
+++ 0. Class/w7 1.py
@@ -0,0 +1,7 @@
+name='name'
+
+input1=int(input('tell me your name: '))
+print(input1) 
+print(type(input1))
+output2=input1*10
+print(output2)
---
+++ 0. Class/w7 2.py
@@ -0,0 +1,4 @@
+input2=str(input('tell me your ic?'))
+print(input2)
+print(type(input2))
+print('last digit is',input2[11])
