d5de09c10dfc697b85a6b71458f132597bfe2ae0
Kai Xian <chewkxmy@gmail.com>
2024-05-09 09:29:28 +0800

    update class

--- 0. Class/w7 2.py
+++ 0. Class/w7 2.py
@@ -2,3 +2,6 @@ input2=str(input('tell me your ic?'))
 print(input2)
 print(type(input2))
 print('last digit is',input2[11])
+# 0 indicate the first string
+print('state is ',input2[6:8])
+# means taking 7th string, don't take 9th string
