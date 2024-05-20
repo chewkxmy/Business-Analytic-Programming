ec64e201c24fe159ba2b5d39a8036c10a663d40c
Kai Xian <chewkxmy@gmail.com>
2024-05-20 09:48:01 +0800

    class

---
+++ 0. Class/w7 5.py
@@ -0,0 +1,8 @@
+a=int(input('First number: '))
+b=int(input('Secound number: '))
+def multiplication(a,b):
+    c=a*b
+    return c
+for number in range(0,3,1):
+    d=multiplication(a,b)
+    print(d)
