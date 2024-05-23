a0c92e59178ae3d5b9ff68ea4bed829ae6bce16c
Kai Xian <chewkxmy@gmail.com>
2024-05-23 09:43:30 +0800

    class

---
+++ 0. Class/w8 2/logic.py
@@ -0,0 +1,7 @@
+def area_circle(radius):
+    area=(22/7)*(radius**2)
+    return area
+
+def cir_circle(radius):
+    cir=(22/7)*(radius)*2
+    return cir
---
+++ 0. Class/w8 2/main.py
@@ -0,0 +1,7 @@
+from logic import area_circle
+
+user_input=float(input('enter number: '))
+
+answer=area_circle(user_input)
+
+print(answer)
