4faadf1424b84034c3ea7b6aa0f4d96adb2560e4
Kai Xian <chewkxmy@gmail.com>
2024-06-27 09:37:26 +0800

    class

--- 0. Class/w12_Dashboard/first_page.py
+++ 0. Class/w12_Dashboard/first_page.py
@@ -1,1 +1,14 @@
+import streamlit as st
+
 def page1():
+    st.title("Calculator Program")
+    st.text("This is a calculator program and it received input and calculate it")
+
+    with st.form("my_form"):
+        a=st.number_input("1st Number")
+        b=st.number_input("2nd Number")
+        submitted = st.form_submit_button("Submit")
+
+        if submitted:
+            c=a*b
+            st.write("Answer = ",c)
--- 0. Class/w12_Dashboard/second_page.py
+++ 0. Class/w12_Dashboard/second_page.py
@@ -1,1 +1,5 @@
+import streamlit as st
+
 def page2():
+    st.title("Page 2")
+    st.write("This is page 2")
